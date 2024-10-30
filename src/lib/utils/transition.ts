316bbe6a6daa14343292f96fe3c69cb65d60cefb
nutchyleo <nutchapon.m@thaibev.com>
2024-10-30 21:41:50 +0700

    fix: update some duration of transition to make a game work smoothly

--- src/lib/utils/transition.ts
+++ src/lib/utils/transition.ts
@@ -2,6 +2,6 @@ import { cubicOut } from 'svelte/easing';
 import { crossfade } from 'svelte/transition';
 
 export const [send, receive] = crossfade({
-	duration: 500,
+	duration: 250,
 	easing: cubicOut
 });
